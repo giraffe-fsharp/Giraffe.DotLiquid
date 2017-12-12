568b47f5c307c508a62cb2865c43f2a92b12aeae
Dustin Moris Gorski <dustinmoris@gmail.com>
2017-12-12 12:21:43 +0000

    Moved Giraffe.DotLiquid code and tests
    ... and made a minor change to a function.

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,12 @@
+Release Notes
+=============
+
+## Develop branch
+
+#### Breaking changes
+
+- Renamed `dotLiquidHtmlView` to `dotLiquidHtmlTemplate` for consistency
+
+## 0.1.0-beta-200 and before
+
+Previous releases of this library were documented in [Giraffe's release notes](https://github.com/giraffe-fsharp/Giraffe/blob/master/RELEASE_NOTES.md).
---
+++ samples/GiraffeDotLiquidSample.Tests/Tests.fs
@@ -0,0 +1,104 @@
+module GiraffeDotLiquidSample.Tests
+
+open System
+open System.Net.Http
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.TestHost
+open Xunit
+
+// ---------------------------------
+// Test server/client setup
+// ---------------------------------
+
+let createHost() =
+    WebHostBuilder()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .Configure(Action<IApplicationBuilder> GiraffeDotLiquidSample.configureApp)
+
+// ---------------------------------
+// Helper functions
+// ---------------------------------
+
+let runTask task =
+    task
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+let get (client : HttpClient) (path : string) =
+    path
+    |> client.GetAsync
+    |> runTask
+
+let ensureSuccess (response : HttpResponseMessage) =
+    if not response.IsSuccessStatusCode
+    then response.Content.ReadAsStringAsync() |> runTask |> failwithf "%A"
+    else response
+
+let isOfType (contentType : string) (response : HttpResponseMessage) =
+    Assert.Equal(contentType, response.Content.Headers.ContentType.MediaType)
+    response
+
+let readText (response : HttpResponseMessage) =
+    response.Content.ReadAsStringAsync()
+    |> runTask
+
+let shouldEqual expected actual =
+    Assert.Equal(expected, actual)
+
+// ---------------------------------
+// Tests
+// ---------------------------------
+
+[<Fact>]
+let ``Test /person returns html content`` () =
+    use server = new TestServer(createHost())
+    use client = server.CreateClient()
+    let expected = @"<html><head><title>DotLiquid</title></head><body><p>First name: Foo<br />Last name: Bar</p></body></html>"
+
+    get client "/person"
+    |> ensureSuccess
+    |> isOfType "text/html"
+    |> readText
+    |> shouldEqual expected
+
+[<Fact>]
+let ``Test /person2 returns html content`` () =
+    use server = new TestServer(createHost())
+    use client = server.CreateClient()
+
+    let expected = @"<html>
+<head>
+    <title>DotLiquid</title>
+</head>
+<body>
+    <p><strong>First name:</strong> Foo<br /><strong>Last name:</strong> Bar</p>
+</body>
+</html>"
+
+    get client "/person2"
+    |> ensureSuccess
+    |> isOfType "text/html"
+    |> readText
+    |> shouldEqual expected
+
+[<Fact>]
+let ``Test /person3 returns html content`` () =
+    use server = new TestServer(createHost())
+    use client = server.CreateClient()
+
+    let expected = @"<html>
+<head>
+    <title>DotLiquid</title>
+</head>
+<body>
+    <p><strong>First name:</strong> Foo<br /><strong>Last name:</strong> Bar</p>
+</body>
+</html>"
+
+    get client "/person3"
+    |> ensureSuccess
+    |> isOfType "text/html"
+    |> readText
+    |> shouldEqual expected
